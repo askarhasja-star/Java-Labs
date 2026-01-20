feac5e8ab890c0e4a908afb5968d864c6f59f7b1
Askar <askarhasja@gmail.com>
2026-01-20 14:16:31 +0500

    Добавлен исходный код Java-программы

---
+++ Lab 1/src/com/example/StudentInfo.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        String fullName = "Нурхадыр Аскар Нурланулы";
+        String group = "ИС-21 Типо";
+        String program = "Информационные системы";
+
+        System.out.println("=== Информация о студенте ===");
+        System.out.println("ФИО: " + fullName);
+        System.out.println("Группа: " + group);
+        System.out.println("ОП/Специальность: " + program);
+    }
+}
