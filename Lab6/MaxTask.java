7c1e4d3fe57d31f19faa5bdf002cdcadfa72487a
askarhasja-star <askarhasja@gmail.com>
2026-02-24 09:18:07 +0000

    Add Lab6

--- Lab6/MaxTask.java
+++ Lab6/MaxTask.java
@@ -1,19 +1,52 @@
+import java.util.Scanner;
+
 public class MaxTask {
 
+    // Для int
     public static int max(int a, int b) {
         return (a > b) ? a : b;
     }
 
+    // Для double
     public static double max(double a, double b) {
         return (a > b) ? a : b;
     }
 
     public static void main(String[] args) {
 
-        int intResult = max(10, 25);
-        double doubleResult = max(5.7, 3.2);
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Максимум (int): " + intResult);
-        System.out.println("Максимум (double): " + doubleResult);
+        System.out.print("Введите первое целое число: ");
+        if (!sc.hasNextInt()) {
+            System.out.println("Ошибка: нужно ввести целое число!");
+            return;
+        }
+        int a = sc.nextInt();
+
+        System.out.print("Введите второе целое число: ");
+        if (!sc.hasNextInt()) {
+            System.out.println("Ошибка: нужно ввести целое число!");
+            return;
+        }
+        int b = sc.nextInt();
+
+        System.out.println("Максимум (int): " + max(a, b));
+
+        // ----- Ввод double -----
+        System.out.print("Введите первое дробное число: ");
+        if (!sc.hasNextDouble()) {
+            System.out.println("Ошибка: нужно ввести число!");
+            return;
+        }
+        double x = sc.nextDouble();
+
+        System.out.print("Введите второе дробное число: ");
+        if (!sc.hasNextDouble()) {
+            System.out.println("Ошибка: нужно ввести число!");
+            return;
+        }
+        double y = sc.nextDouble();
+
+        System.out.println("Максимум (double): " + max(x, y));
     }
 }
