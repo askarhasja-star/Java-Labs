51012977fa309246964fb0284955bc13a05b0c54
askarhasja-star <askarhasja@gmail.com>
2026-03-17 09:14:21 +0000

    Add Lab9

--- Lab9/Task1_Transport.java
+++ Lab9/Task1_Transport.java
@@ -10,36 +10,74 @@ class Transport {
     }
 
     void move() {
-        System.out.println(name + " движется");
+        System.out.println(name + " движется со скоростью " + speed + " км/ч");
+    }
+
+    String getType() {
+        return "Транспорт";
     }
 }
 
 class Car extends Transport {
-    Car(String n, int s) { super(n, s); }
-    void move() { System.out.println(name + " едет по дороге"); }
+    Car(String name, int speed) { super(name, speed); }
+
+    void move() {
+        System.out.println(name + " (" + getType() + ") едет по дороге со скоростью " + speed + " км/ч");
+    }
+
+    String getType() { return "Машина"; }
 }
 
 class Train extends Transport {
-    Train(String n, int s) { super(n, s); }
-    void move() { System.out.println(name + " едет по рельсам"); }
+    Train(String name, int speed) { super(name, speed); }
+
+    void move() {
+        System.out.println(name + " (" + getType() + ") едет по рельсам со скоростью " + speed + " км/ч");
+    }
+
+    String getType() { return "Поезд"; }
 }
 
 class Airplane extends Transport {
-    Airplane(String n, int s) { super(n, s); }
-    void move() { System.out.println(name + " летит"); }
+    Airplane(String name, int speed) { super(name, speed); }
+
+    void move() {
+        System.out.println(name + " (" + getType() + ") летит со скоростью " + speed + " км/ч");
+    }
+
+    String getType() { return "Самолёт"; }
 }
 
 public class Task1_Transport {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Название: ");
-        String name = sc.next();
+        System.out.print("Название машины: ");
+        String carName = sc.nextLine();
+        System.out.print("Скорость машины: ");
+        int carSpeed = sc.nextInt();
+        sc.nextLine(); // очищаем буфер после числа
 
-        System.out.print("Скорость: ");
-        int speed = sc.nextInt();
+        System.out.print("Название поезда: ");
+        String trainName = sc.nextLine();
+        System.out.print("Скорость поезда: ");
+        int trainSpeed = sc.nextInt();
+        sc.nextLine();
 
-        Transport t = new Car(name, speed);
-        t.move();
+        System.out.print("Название самолёта: ");
+        String airplaneName = sc.nextLine();
+        System.out.print("Скорость самолёта: ");
+        int airplaneSpeed = sc.nextInt();
+
+        Transport[] transports = {
+            new Car(carName, carSpeed),
+            new Train(trainName, trainSpeed),
+            new Airplane(airplaneName, airplaneSpeed)
+        };
+
+        System.out.println("\nДвижение транспорта:");
+        for (Transport t : transports) {
+            t.move();
+        }
     }
 }
