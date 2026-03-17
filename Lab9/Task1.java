4a8bc2c20ac0609bbb416818e520312e878e6859
askarhasja-star <askarhasja@gmail.com>
2026-03-17 09:44:43 +0000

    Add Lab9

--- Lab9/Task1.java
+++ Lab9/Task1.java
@@ -1,20 +1,22 @@
 class Car {
     String brand;
     int speed;
+    String country;
 
-    Car(String brand, int speed) {
+    Car(String brand, int speed, String country) {
         this.brand = brand;
         this.speed = speed;
+        this.country = country;
     }
 
     void displayInfo() {
-        System.out.println("Марка: " + brand + ", Скорость: " + speed);
+        System.out.println("Марка: " + brand + ", Скорость: " + speed + ", Страна: " + country);
     }
 }
 
 class Sedan extends Car {
-    Sedan(String brand, int speed) {
-        super(brand, speed);
+    Sedan(String brand, int speed, String country) {
+        super(brand, speed, country);
     }
 
     void fuelConsumption() {
@@ -23,8 +25,8 @@ class Sedan extends Car {
 }
 
 class Truck extends Car {
-    Truck(String brand, int speed) {
-        super(brand, speed);
+    Truck(String brand, int speed, String country) {
+        super(brand, speed, country);
     }
 
     void fuelConsumption() {
@@ -33,8 +35,8 @@ class Truck extends Car {
 }
 
 class SUV extends Car {
-    SUV(String brand, int speed) {
-        super(brand, speed);
+    SUV(String brand, int speed, String country) {
+        super(brand, speed, country);
     }
 
     void fuelConsumption() {
@@ -44,9 +46,9 @@ class SUV extends Car {
 
 public class Task1 {
     public static void main(String[] args) {
-        Sedan s = new Sedan("Toyota", 180);
-        Truck t = new Truck("Volvo", 120);
-        SUV suv = new SUV("BMW", 160);
+        Sedan s = new Sedan("Toyota", 180, "Japan");
+        Truck t = new Truck("Mercedes", 120, "German");
+        SUV suv = new SUV("BMW", 160, "German");
 
         s.displayInfo(); s.fuelConsumption();
         t.displayInfo(); t.fuelConsumption();
